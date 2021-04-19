50e5aba3855f7448a86f098e27ecc849cf7645d9
Eduard <mail@rostkov.pro>
2021-04-19 09:59:37 +0400

    Задача 4.2

--- src/me/rostkov/lab/Main.java
+++ src/me/rostkov/lab/Main.java
@@ -20,6 +20,7 @@ import me.rostkov.lab.task.chapter_3.unit_3.CityTask;
 import me.rostkov.lab.task.chapter_3.unit_4.LinkedTrackTask;
 import me.rostkov.lab.task.chapter_3.unit_5.ArticleTask;
 import me.rostkov.lab.task.chapter_4.unit_1.StrictlyPointTask;
+import me.rostkov.lab.task.chapter_4.unit_2.StrictlyStraightTask;
 import me.rostkov.lab.task.extra.extra_1.ExtraInterfaceTask;
 import me.rostkov.lab.task.extra.extra_2.CalendarTask;
 
@@ -113,6 +114,7 @@ public class Main {
         Main.taskManager.addTask(new LinkedTrackTask());
         Main.taskManager.addTask(new ArticleTask());
         Main.taskManager.addTask(new StrictlyPointTask());
+        Main.taskManager.addTask(new StrictlyStraightTask());
 
         Main.taskManager.addTask(new ExtraInterfaceTask(), false);
         Main.taskManager.addTask(new CalendarTask(), false);
---
+++ src/me/rostkov/lab/task/chapter_4/unit_2/StrictlyStraight.java
@@ -0,0 +1,41 @@
+package me.rostkov.lab.task.chapter_4.unit_2;
+
+import me.rostkov.lab.task.chapter_1.unit_1.Point;
+import me.rostkov.lab.task.chapter_4.unit_1.StrictlyPoint;
+
+public class StrictlyStraight {
+    StrictlyPoint point1;
+    StrictlyPoint point2;
+
+    public StrictlyStraight(StrictlyPoint point1, StrictlyPoint point2) {
+        this.point1 = point1;
+        this.point2 = point2;
+    }
+
+    public StrictlyStraight(int x1, int y1, int x2, int y2) {
+        this.point1 = new StrictlyPoint(x1, y1);
+        this.point2 = new StrictlyPoint(x2, y2);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Линия от %s до %s", this.point1, this.point2);
+    }
+
+    public StrictlyPoint getPoint1() {
+        return point1;
+    }
+
+    public void setPoint1(StrictlyPoint point1) {
+        this.point1 = point1;
+    }
+
+    public StrictlyPoint getPoint2() {
+        return point2;
+    }
+
+    public void setPoint2(StrictlyPoint point2) {
+        this.point2 = point2;
+    }
+
+}
